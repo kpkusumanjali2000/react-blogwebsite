f7f595911c0e825293a7c3e88aa7117b36ae139d
kpkusumanjali2000 <kusumanjalikp1995@gmail.com>
2023-05-29 19:54:23 +0530

    feat: :tada: initial commits added topbar

---
+++ blogwebsite/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Topbar from "../src/components/topbar.js";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      </Router>
+    
+  );
+}
+
+export default App;
